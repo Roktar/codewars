e5496c577e69329c31b9e22f84aa46b3bfbf4278
Roktar <outofsight_@naver.com>
2018-01-25 19:49:53 +0900

    Numbers in strings - 18. 01. 25

---
+++ Numbers_in_strings.py
@@ -0,0 +1,24 @@
+def solve(s) :
+    arr = list()
+    sub = ""
+    
+    for d in s :
+        print(d)
+        if ord(d) >= 48 and ord(d) <= 57 :
+            sub += d
+            continue
+        arr.append(sub)
+        sub = ""
+    arr.append(sub)
+
+    while True :
+        if '' in arr :
+            arr.remove('')
+        else:
+            break
+
+    max = 0
+    for no in arr :
+        if int(no) > max :
+            max = int(no)
+    return max
