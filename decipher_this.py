d5fbd29d679e3d4bd7fe344d72570c2f243725bb
Roktar <outofsight_@naver.com>
2018-02-04 21:03:10 +0900

    Decipher This! - 18. 02. 04

--- decipher_this.py
+++ decipher_this.py
@@ -6,8 +6,6 @@ def decipher_this(string):
             if 48 <= ord(s[i]) <= 57 :
                 if i == len(s) -1 :
                     text.append(chr(int(s)) + ' ')
-                else :
-                    continue
             else :
                 sub = [s2 for s2 in s[i:]]
                 tmp = ""
@@ -17,25 +15,17 @@ def decipher_this(string):
                     tmp = sub[0]
                     sub[0] = sub[len(sub) -1]
                     sub[len(sub) -1] = tmp
-                    tmp = ""
-
-                    for s2 in sub :
-                        tmp += s2 
+                    tmp = "".join(sub)
                 else :
                     for i in range(len(sub)) :
                         tmp += sub[-i-1]
 
                 text.append(chr(int(num)) + tmp + ' ')
                 break
-
     d = text.pop().split(' ')
     text.append(d[0])
 
-    s = ""
-    for i in text :
-        s += i 
+    return "".join(text)
 
-    return s
-
-#d = decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka")
-#print(d)
+d = decipher_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka")
+print(d)
