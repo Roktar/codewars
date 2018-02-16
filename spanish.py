b91270da75e7719a913a0560c1e691dcc9a13e97
Roktar <outofsight_@naver.com>
2018-02-16 13:35:31 +0900

    Spanish Conjugator - 18. 02. 16

--- spanish.py
+++ spanish.py
@@ -4,11 +4,7 @@ def conjugate(verb):
                 "er", [ "o", "es", "e", "emos", "eis", "en" ], 
                 "ir", [ "o", "es", "e", "imos", "is", "en" ]
              ]
-    s_set = list()
     
-    for s in suffix[suffix.index(verb[len(verb)-2:]) +1] :
-        s_set.append(verb[:len(verb)-2] + s)
-    
-    return {verb : s_set}
+    return {verb : [ verb[:len(verb) -2] + s for s in suffix[suffix.index(verb[len(verb)-2:]) +1] ]}
 
 print(conjugate("caminar"))
