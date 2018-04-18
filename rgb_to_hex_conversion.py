4e5738333bf29706ae175639db5465494b05a595
Roktar <outofsight_@naver.com>
2018-04-18 11:19:17 +0900

    RGB to Hex Conversion - 18. 04. 18

--- rgb_to_hex_conversion.py
+++ rgb_to_hex_conversion.py
@@ -1,7 +1,4 @@
 def rgb(r, g, b):
-    return getHexData(r, g, b)
-    
-def getHexData(r, g, b) :
     return calculation(r) + calculation(g) + calculation(b)
 
 def calculation(data) :
@@ -10,14 +7,10 @@ def calculation(data) :
     if data < 1 :
         return "00"
     
-    if data > 15 :
-        return switch( int(data/16) ) + switch(data%16)
-    else :
-        return '0' + switch(data%16)
-    
+    return switch( int(data/16) ) + switch(data%16)
+
 def switch(data) :
     return {
-            0 : '0',
             1 : '1',
             2 : '2',
             3 : '3',
@@ -33,6 +26,7 @@ def switch(data) :
             13 : 'D',
             14 : 'E',
             15 : 'F'
-            }.get(data, '-1')
+            }.get(data, '0')
 
 print(rgb(254,253,222))
+print(rgb(1,2,3))
