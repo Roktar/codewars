bfd0c29af7a2df59f71cdbb977836d7e02caff4a
Roktar <outofsight_@naver.com>
2018-09-14 16:53:32 +0900

    Simple Fun #40: Timed Reading - 18. 09. 14

--- lang/javascript/simple_fun_#40.js
+++ lang/javascript/simple_fun_#40.js
@@ -1,20 +1,14 @@
 function timedReading(maxLength, text) {
     let arr = text.split(" ");
-    let resArr = [];
-  
-    for(let i in arr) {
-      c(arr, i, ",", "\'", ".", "?", "!");
-      if(arr[i] && arr[i].length <= maxLength)
-        resArr.push(arr[i]);
-    }
-    return resArr.length;
+    
+    return arr.filter( (v, i) => c(arr, i, ",", "\'", ".", "?", "!") || (arr[i] && arr[i].length <= maxLength) ).length;
   }
   
-  function c(arr, i, ...splitChars) {
-    for(let ch of splitChars) {
-      if(arr[i].includes(ch)) {
-        let s = arr[i].split(ch);
-        arr[i] = s[0] || s[1];
-      }
+function c(arr, i, ...splitChars) {
+  for(let ch of splitChars) {
+    if(arr[i].includes(ch)) {
+      let s = arr[i].split(ch);
+      arr[i] = s[0] || s[1];
     }
   }
+}
