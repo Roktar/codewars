02a5e1f6372272309dc3a5c21c9d6c24ed04c3dd
Roktar <loadcaser@gmail.com>
2021-06-17 01:27:18 +0900

    Common directory path - 21. 06. 17

--- lang/javascript/2021/06/[210617]common_directory_path/common_directory_path.js
+++ lang/javascript/2021/06/[210617]common_directory_path/common_directory_path.js
@@ -1,4 +1,25 @@
-function anagrams(word, words) {
-  let str = [...word].sort().join``;
-  return words.filter( v => str === [...v].sort().join`` );
+function getCommonDirectoryPath(pathes) {
+  let checkFirstSlash = pathes.every( v => /^\//.exec(v) );
+  let checkFirstNonSlash = pathes.every( v => /^\w/.exec(v) );
+  let idx = (checkFirstSlash && !checkFirstNonSlash ? 1 : 0);
+  let arr = (checkFirstSlash && !checkFirstNonSlash ? [''] : []);
+  
+  if(checkFirstSlash == false && checkFirstNonSlash == false)
+    return '';
+  
+  let subPathSet = pathes.map( v => v.split`/`);
+  
+  return getDirectory(subPathSet, idx, subPathSet[0].length, arr).concat('').join`/`;
+}
+
+function getDirectory(subPathSet, idx, len, arr) {
+  if( idx > len)
+    return arr;
+
+  if( !subPathSet.every(sub => sub[idx] === subPathSet[0][idx]) )
+    return arr;
+  else {
+    arr.push(subPathSet[0][idx]);
+    return getDirectory(subPathSet, idx+1, len, arr);
+  }
 }
